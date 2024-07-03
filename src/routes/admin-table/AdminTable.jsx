0173dfcb3d97388ef109fc6e362e82c3c84a4777
tin huynh <tin.huynh@madison-technology.com>
2024-07-03 08:54:04 +0700

    tmp

--- src/routes/admin-table/AdminTable.jsx
+++
@@ -1,9 +0,0 @@
-import BookingTable from 'components/admin-table/AdminTable';
-
-export const AdminTable = () => {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <BookingTable />
-    </div>
-  );
-};
